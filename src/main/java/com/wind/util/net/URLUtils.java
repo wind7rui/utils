e2847f8441e51920b4b2b579043c6ebf26a6dfdf
wind7rui <wangtian.rui@163.com>
2017-10-21 14:09:36 +0800

    URLUtils方法添加注释

--- src/main/java/com/wind/util/net/URLUtils.java
+++ src/main/java/com/wind/util/net/URLUtils.java
@@ -1,6 +1,7 @@
 package com.wind.util.net;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -14,27 +15,52 @@ import java.net.URLEncoder;
  */
 public class URLUtils {
 
+    /**
+     * @param url <code>String</code> to be translated.
+     * @return the translated <code>String</code>.
+     */
     public static String encodeToUTF8(String url) {
-        try {
-            return URLEncoder.encode(url, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Encode to UTF8 failed", e);
-        }
+        return encode(url, "UTF-8");
     }
 
-    public static String encode(String value, String charSet) {
+    /**
+     * @param url     <code>String</code> to be translated.
+     * @param charSet The name of a supported
+     *                <a href="../lang/package-summary.html#charenc">character
+     *                encoding</a>.
+     * @return the translated <code>String</code>.
+     */
+    public static String encode(String url, String charSet) {
         try {
-            return URLEncoder.encode(value, charSet);
+            return URLEncoder.encode(url, charSet);
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
 
+    /**
+     * @param encodeUrl the <code>String</code> to decode
+     * @return the newly decoded <code>String</code>
+     */
     public static String decodeToUTF8(Optional<String> encodeUrl) {
+        return decode(encodeUrl, "UTF-8");
+    }
+
+    /**
+     * @param encodeUrl the <code>String</code> to decode
+     * @param charSet   The name of a supported
+     *                  <a href="../lang/package-summary.html#charenc">character
+     *                  encoding</a>.
+     * @return the newly decoded <code>String</code>
+     */
+    public static String decode(Optional<String> encodeUrl, String charSet) {
+        if (Strings.isNullOrEmpty(charSet)) {
+            charSet = "UTF-8";
+        }
         try {
-            return !encodeUrl.isPresent() ? "" : URLDecoder.decode(encodeUrl.get(), "UTF-8");
+            return !encodeUrl.isPresent() ? "" : URLDecoder.decode(encodeUrl.get(), charSet);
         } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Decode to UTF8 failed", e);
+            throw new RuntimeException("Decode to " + charSet + " failed", e);
         }
     }
 
